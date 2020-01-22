5e9640044e93326740ad4bc197f2cbfded0e4070
Sameer Bhat <sameerbhat76@gmail.com>
2020-01-22 23:50:39 +0530

    bugs

--- src/main/java/HiveJdbcTest.java
+++ src/main/java/HiveJdbcTest.java
@@ -2,32 +2,65 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 class HiveJdbcTest {
   public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+    Connection connection = null;
+    Statement statement = null;
+    ResultSet resultSet = null;
+
+
     if (args.length != 3) {
       System.err.println("Usage: HiveJdbcTest <jdbc-uri> <user> <password>");
       System.exit(-1);
     }
 
+
     Class.forName("org.apache.hive.jdbc.HiveDriver");
 
-    Connection connection = DriverManager
-        .getConnection(args[0], args[1], args[2]);
+     connection = DriverManager.getConnection(args[0], args[1], args[2]);
 
-    ResultSet resultSet = connection
-        .getMetaData()
-        .getTables(null, "default", null, new String[] { "TABLE" });
 
-    while (resultSet.next()) {
-      System.out.printf("%s, %s, %s, %s, %s\n",
-          resultSet.getString("TABLE_CAT"),
-          resultSet.getString("TABLE_SCHEM"),
-          resultSet.getString("TABLE_NAME"),
-          resultSet.getString("TABLE_TYPE"),
-          resultSet.getString("REMARKS"));
-    }
+        try{
 
-    connection.close();
+          connection=DriverManager.getConnection
+             (args[0], args[1], args[2]);
+
+             // first arg is : DB_URL jdbc:hive2://localhost:3306/databasebname
+             // second arg is: DB_USER 
+             // third arg is : DB_PASSWD
+
+             // java -jar  app.jar arg1 arg2 arg3
+          
+          
+          
+          statement=connection.createStatement();
+          
+          resultSet=statement.executeQuery("SELECT * FROM books");
+          
+          while(resultSet.next()){
+              
+             System.out.printf("%s\t%s\t%s\t%f\n",
+                     
+             resultSet.getString(1),
+             resultSet.getString(2),
+             resultSet.getString(3),
+             resultSet.getFloat(4));
+          }
+ 
+          
+       }catch(SQLException ex){
+       }finally{
+          try {
+             resultSet.close();
+             statement.close();
+             connection.close();
+          } catch (SQLException ex) {
+          }
+       }
+
+
   }
 }
---
+++ src/main/java/HiveJdbcTest_bkp.java
@@ -0,0 +1,33 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+class HiveJdbcTest1 {
+  public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    if (args.length != 3) {
+      System.err.println("Usage: HiveJdbcTest <jdbc-uri> <user> <password>");
+      System.exit(-1);
+    }
+
+    Class.forName("org.apache.hive.jdbc.HiveDriver");
+
+    Connection connection = DriverManager
+        .getConnection(args[0], args[1], args[2]);
+
+    ResultSet resultSet = connection
+        .getMetaData()
+        .getTables(null, "default", null, new String[] { "TABLE" });
+
+    while (resultSet.next()) {
+      System.out.printf("%s, %s, %s, %s, %s\n",
+          resultSet.getString("TABLE_CAT"),
+          resultSet.getString("TABLE_SCHEM"),
+          resultSet.getString("TABLE_NAME"),
+          resultSet.getString("TABLE_TYPE"),
+          resultSet.getString("REMARKS"));
+    }
+
+    connection.close();
+  }
+}
